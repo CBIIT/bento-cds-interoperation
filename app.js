ced5fa5241c449a9b4b1dcbc898d6dfd69992761
David-YuWei <ywwei85@gmail.com>
2025-08-01 20:33:45 -0400

    Add OPTIONS to the allow methods

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use((req, res, next) => {
     }
     logger.debug("Request allowed");
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
     next();
